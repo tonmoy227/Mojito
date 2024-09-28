52827e81a444e5d2261f87688103c7460c89d5cc
tonmoy227 <145573783+tonmoy227@users.noreply.github.com>
2024-09-28 16:58:22 +0600

    Hero 2.0
    Hero 2.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -315,4 +315,16 @@ Faq Active
 		pauseOnHover: true,
 		startVisible:true,
 	});
+	if ($(".progress-bar").length) {
+		var $progress_bar = $('.progress-bar');
+		$progress_bar.appear();
+		$(document.body).on('appear', '.progress-bar', function() {
+			var current_item = $(this);
+			if (!current_item.hasClass('appeared')) {
+				var percent = current_item.data('percent');
+				current_item.css('width', percent + '%').addClass('appeared').parent().append('<span>' + percent + '%' + '</span>');
+			}
+
+		});
+	};
 })(jQuery);
